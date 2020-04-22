b39f531104f33c5e6c057cc810abedd7076b9926
Elvira <elvi_ac94@hotmail.com>
2020-04-22 17:48:25 +0200

    add folders structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../stylesheet/App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        hola
+      </div>
+    );
+  }
+}
+
+export default App;
