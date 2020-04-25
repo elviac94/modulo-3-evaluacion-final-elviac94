6402451c79c6562094830d434e226232c73a0cd9
Elvira <elvi_ac94@hotmail.com>
2020-04-25 21:25:11 +0200

    docs

---
+++ docs/precache-manifest.1b5ae5da4189f2538996d7a9914350d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d17558a6fdfd6f09ee592fe850e2432",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d8f9cba2e0fa5665b41",
+    "url": "./static/css/main.899d1c74.chunk.css"
+  },
+  {
+    "revision": "8e21e9e3e57dcbc6801e",
+    "url": "./static/js/2.8c80a218.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.8c80a218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d8f9cba2e0fa5665b41",
+    "url": "./static/js/main.2aeacc19.chunk.js"
+  },
+  {
+    "revision": "44a19bde7ca20ce894f4",
+    "url": "./static/js/runtime-main.3a7121df.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick_and_Morty_-_logo_(English).75509f2e.png"
+  }
+]);
